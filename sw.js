d22a289a5da7fd428743b1749d2912a369043c9d
tomtomyoung <yangpeizhuo1997@163.com>
2021-11-12 10:01:05 +0800

    blog update

--- sw.js
+++ sw.js
@@ -1,17 +1,40 @@
 const CACHE_VERSION = 1;
 
 const BASE_CACHE_FILES = [
+    // page cache
+    '/',
+    '/post',
+    '/categories',
+    '/tags',
+    '/links',
+    // assets cache
     '/css/style.min.css',
     '/js/util.min.js',
     '/manifest.json',
+    // icon cache
     '/favicons/favicon.ico',
 ];
 
 const OFFLINE_CACHE_FILES = [
+    // page cache
+    '/404.html',
+    // assets cache
+    '/css/style.min.css',
+    '/js/util.min.js',
+    '/manifest.json',
+    // icon cache
+    '/favicons/favicon.ico'
 ];
 
 const NOT_FOUND_CACHE_FILES = [
+    // page cache
     '/404.html',
+    // assets cache
+    '/css/style.min.css',
+    '/js/util.min.js',
+    '/manifest.json',
+    // icon cache
+    '/favicons/favicon.ico'
 ];
 
 const OFFLINE_PAGE = '/index.html';
@@ -34,9 +57,9 @@ const MAX_TTL = {
 };
 
 const CACHE_BLACKLIST = [
-    //(str) => {
-    //    return !str.startsWith('http://localhost') && !str.startsWith('https://gohugohq.com');
-    //},
+    (str) => {
+       return !str.startsWith('http://localhost');
+    },
 ];
 
 const SUPPORTED_METHODS = [
@@ -50,7 +73,7 @@ const SUPPORTED_METHODS = [
  */
 function isBlacklisted(url) {
     return (CACHE_BLACKLIST.length > 0) ? !CACHE_BLACKLIST.filter((rule) => {
-        if(typeof rule === 'function') {
+        if (typeof rule === 'function') {
             return !rule(url);
         } else {
             return false;
@@ -122,14 +145,12 @@ function installServiceWorker() {
  * @returns {Promise}
  */
 function cleanupLegacyCache() {
-
     let currentCaches = Object.keys(CACHE_VERSIONS)
         .map(
             (key) => {
                 return CACHE_VERSIONS[key];
             }
         );
-
     return new Promise(
         (resolve, reject) => {
 
@@ -179,12 +200,12 @@ function cleanupLegacyCache() {
 }
 
 function precacheUrl(url) {
-    if(!isBlacklisted(url)) {
+    if (!isBlacklisted(url)) {
         caches.open(CACHE_VERSIONS.content)
             .then((cache) => {
                 cache.match(url)
                     .then((response) => {
-                        if(!response) {
+                        if (!response) {
                             return fetch(url)
                         } else {
                             // already in cache, nothing to do.
@@ -192,7 +213,7 @@ function precacheUrl(url) {
                         }
                     })
                     .then((response) => {
-                        if(response) {
+                        if (response) {
                             return cache.put(url, response.clone());
                         } else {
                             return null;
@@ -283,7 +304,6 @@ self.addEventListener(
                                                                     resolve(response);
                                                                 }
                                                             );
-
                                                     }
                                                 )
                                                     .catch(
@@ -313,9 +333,15 @@ self.addEventListener(
                                             .then(
                                                 (response) => {
 
-                                                    if(response.status < 400) {
+                                                    if (response.status < 400) {
                                                         if (~SUPPORTED_METHODS.indexOf(event.request.method) && !isBlacklisted(event.request.url)) {
                                                             cache.put(event.request, response.clone());
+                                                            // if (request.url.match("^(http|https)://")) {
+                                                            //     // cache.put(request, copy);
+                                                            //     cache.put(event.request, response.clone());
+                                                            // } else {
+                                                            //     return;
+                                                            // }
                                                         }
                                                         return response;
                                                     } else {
@@ -326,20 +352,18 @@ self.addEventListener(
                                                 }
                                             )
                                             .then((response) => {
-                                                if(response) {
+                                                if (response) {
                                                     return response;
                                                 }
                                             })
                                             .catch(
                                                 () => {
-
                                                     return caches.open(CACHE_VERSIONS.offline)
                                                         .then(
                                                             (offlineCache) => {
                                                                 return offlineCache.match(OFFLINE_PAGE)
                                                             }
                                                         )
-
                                                 }
                                             );
                                     }
@@ -361,15 +385,15 @@ self.addEventListener(
 
 self.addEventListener('message', (event) => {
 
-    if(
+    if (
         typeof event.data === 'object' &&
         typeof event.data.action === 'string'
     ) {
-        switch(event.data.action) {
-            case 'cache' :
+        switch (event.data.action) {
+            case 'cache':
                 precacheUrl(event.data.url);
                 break;
-            default :
+            default:
                 console.log('Unknown action: ' + event.data.action);
                 break;
         }
